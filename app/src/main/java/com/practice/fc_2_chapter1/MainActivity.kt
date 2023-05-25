b162cd5b7ec26af9c04cbdbc0653184ac0ee122c
syb8200 <syb8200@naver.com>
2023-05-25 17:38:53 +0900

    first commit

---
+++ app/src/main/java/com/practice/fc_2_chapter1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.practice.fc_2_chapter1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
