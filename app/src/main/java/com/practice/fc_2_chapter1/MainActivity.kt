bdbe725e4836b9d2fc4409fb91c8b1afe62f2c51
syb8200 <syb8200@naver.com>
2023-05-25 19:03:13 +0900

    WebView구현

--- app/src/main/java/com/practice/fc_2_chapter1/MainActivity.kt
+++ app/src/main/java/com/practice/fc_2_chapter1/MainActivity.kt
@@ -2,10 +2,22 @@ package com.practice.fc_2_chapter1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import com.practice.fc_2_chapter1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("https://www.naver.com")
+        }
     }
 }
